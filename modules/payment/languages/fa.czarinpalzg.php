caf59831ced378f46013db79c5a0c65983fb5399
MA7 <amini141@gmail.com>
2013-07-10 13:01:40 +0430

    initial

---
+++ modules/payment/languages/fa.czarinpalzg.php
@@ -0,0 +1,25 @@
+<?php 
+/*****************************************************************************
+ *                                                                           *
+ * www.sabadkharid.com                                                       *
+ * Copyright (c) 2008 . All rights reserved.                                 *
+ *                                                                           *
+ *****************************************************************************/
+define('CZARINPALZG_TTL',
+	'زرين پال');
+define('CZARINPALZG_DSCR',
+	'ماژول پرداخت آنلاين زرين پال');
+	
+define('CZARINPALZG_CFG_MERCHANT_ACCOUNT_TTL',
+	'پين کد زرين پال شما');
+define('CZARINPALZG_CFG_MERCHANT_ACCOUNT_DSCR',
+	'لطفا کد زرين پال خود را وارد نماييد');
+	
+define('CZARINPALZG_CFG_RIAL_CURRENCY_TTL',
+	'واحد پول ريال');
+define('CZARINPALZG_CFG_RIAL_CURRENCY_DSCR',
+	'واحد پول ارسالي شما به زرين پال حتما بايد به تومان باشد');
+
+define('CZARINPALZG_TXT_1',
+	'ورود به سايت زرين پال و پرداخت آنلاين');
+?>
